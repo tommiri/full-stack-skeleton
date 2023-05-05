7bd2271ed3ef60f251607acdc1c15091d3b7dad7
Tommi Riiheläinen <tommi.riihelainen@gmail.com>
2023-05-05 19:51:42 +0300

    Added Airbnb rules for linting

---
+++ backend/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'no-console': 0,
+    '@typescript-eslint/comma-dangle': 0,
+    'implicit-arrow-linebreak': 0,
+    'import/no-cycle': 0,
+  },
+};
